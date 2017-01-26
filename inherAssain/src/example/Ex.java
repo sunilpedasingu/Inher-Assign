4e88bfe883d5270fd30e20a711495aa3941e8cf7
sunilpedasingu <psunilkumar103@gmail.com>
2017-01-25 22:03:28 -0500

    hi

---
+++ inherAssain/src/example/Ex.java
@@ -0,0 +1,27 @@
+package example;
+
+public class Ex {
+
+	public static void main(String[] args) {
+		for(int n=1;n<=5;n++)
+		{
+			for(int m=1;m<=n;m++)
+			{
+				System.out.print(n);
+			}System.out.println();
+		}
+		
+		
+		for(int i=5;i>=0;i--)
+		{
+			for(int j=1;j<=i;j++)
+			{
+			System.out.print(i);
+			}
+			System.out.println();
+		}
+
+
+	}
+
+}
